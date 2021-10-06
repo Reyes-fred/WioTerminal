c40aa3243bc6bc35fb195c387074ed1489741106
Alfredo Reyes Montero <reyes-fred@hotmail.com>
2021-10-06 12:45:43 -0500

    Create main.ino

---
+++ MQTT/main.ino
@@ -0,0 +1,138 @@
+/*
+   MQTT - Wio Terminal
+   Author: Alfredo Reyes
+   Date: 05/10/2021
+*/
+
+
+#include "rpcWiFi.h"
+#include"TFT_eSPI.h"
+#include <PubSubClient.h>
+
+
+// Valores de la Red Wifi.
+const char* ssid = "xxxxxxxx"; // WiFi Nombre
+const char* password = "xxxxxxxx";  // WiFi Contraseña
+const char* mqtt_server = "192.168.100.97";  // MQTT Broker URL
+const char* mqtt_user = "xxxxxxx";  // MQTT Broker URL
+const char* mqtt_pass = "xxxxxxxxxxxx";  // MQTT Broker URL
+
+TFT_eSPI tft;
+
+WiFiClient wioClient;
+PubSubClient client(wioClient);
+long lastMsg = 0;
+char msg[50];
+int value = 0;
+
+
+void setup_wifi() {
+
+  delay(10);
+
+  tft.setTextSize(2);
+  tft.setCursor((320 - tft.textWidth("Connecting to Wi-Fi..")) / 2, 120);
+  tft.print("Connecting to Wi-Fi..");
+
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+  WiFi.begin(ssid, password); // Connecting WiFi
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println("");
+  Serial.println("WiFi connected");
+
+  tft.fillScreen(TFT_BLACK);
+  tft.setCursor((320 - tft.textWidth("Connected!")) / 2, 120);
+  tft.print("Connected!");
+
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP()); // Display Local IP Address
+}
+
+void callback(char* topic, byte* payload, unsigned int length) {
+  //tft.fillScreen(TFT_BLACK);
+  Serial.print("Message arrived [");
+  Serial.print(topic);
+  Serial.print("] ");
+  char buff_p[length];
+  for (int i = 0; i < length; i++) {
+    Serial.print((char)payload[i]);
+    buff_p[i] = (char)payload[i];
+  }
+  Serial.println();
+  buff_p[length] = '\0';
+  String msg_p = String(buff_p);
+  tft.fillScreen(TFT_BLACK);
+  tft.setCursor((320 - tft.textWidth("MQTT Message")) / 2, 90);
+  tft.print("MQTT Message: " );
+  tft.setCursor((320 - tft.textWidth(msg_p)) / 2, 120);
+  tft.print(msg_p); // Print receved payload
+
+}
+
+void reconnect() {
+  // Loop until we're reconnected
+  while (!client.connected()) {
+    Serial.print("Attempting MQTT connection...");
+    // Create a random client ID
+    String clientId = "WioTerminal-";
+    clientId += String(random(0xffff), HEX);
+    // Attempt to connect
+    if (client.connect(clientId.c_str(), mqtt_user, mqtt_pass)) {
+      Serial.println("connected");
+      // Once connected, publish an announcement...
+      client.publish("/WioTerminal/TestConnec", "Hola Mundo");
+      // ... and resubscribe
+      client.subscribe("/WioTerminal/Timbre");
+    } else {
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" try again in 5 seconds");
+      // Wait 5 seconds before retrying
+      delay(5000);
+    }
+  }
+}
+
+void setup() {
+
+  tft.begin();
+  tft.fillScreen(TFT_BLACK);
+  tft.setRotation(3);
+  pinMode(WIO_LIGHT, INPUT);
+
+  Serial.println();
+  Serial.begin(115200);
+  setup_wifi();
+  client.setServer(mqtt_server, 1883); // Connect the MQTT Server
+  client.setCallback(callback);
+}
+
+void loop() {
+
+
+  if (!client.connected()) {
+    reconnect();
+  }
+  client.loop();
+
+  long now = millis();
+  if (now - lastMsg > 2000) {
+    lastMsg = now;
+    ++value;
+    snprintf (msg, 50, "Wio Terminal #%ld", value);
+    Serial.print("Publish message: ");
+    Serial.println(msg);
+    int light = analogRead(WIO_LIGHT);
+    Serial.print("Light value:");
+    Serial.println(light);
+    sprintf(msg, "%ld", light);
+    client.publish("/WioTerminal/Light", msg);
+  }
+}
